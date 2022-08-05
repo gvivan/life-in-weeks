25c4a55caac063365d6923ffb8a1ea79ee0c72d3
Vivan Garg <103859388+gvivan@users.noreply.github.com>
2022-08-04 22:47:33 -0400

    add functionality to calculate and display remaining life in weeks

---
+++ main.py
@@ -0,0 +1,11 @@
+age = input("What is your current age?")
+
+years_left = 90 - int(age)
+months_left = years_left*12
+weeks_left = years_left*52
+days_left = years_left*365
+print(f"You have {days_left} days, or {weeks_left} weeks, or {months_left} months left.")
+
+
+
+
